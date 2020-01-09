f9fc224c4a2588a50260ea2c0d9c09b25800f287
duongtrang1 <kuduong.97.tg@gmail.com>
2020-01-09 19:33:18 +0700

    Add project files.

---
+++ OnlineShop/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="Moi nhap Username")]
+        public string UserName { set; get; }
+
+        [Required(ErrorMessage = "Moi nhap Password")]
+        public string PassWord { set; get; }
+
+        public bool RememberMe { set; get; }
+
+    }
+}
